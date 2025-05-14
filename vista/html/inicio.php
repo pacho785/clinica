95feeef659d224e9d0a79ae7ee2d40b08df36c32
pacho785 <nicolath785@gmail.com>
2025-05-14 17:28:41 -0500

    Se realizaron los apartados: asignar,cancelar,consultar e inicio

--- vista/html/inicio.php
+++ vista/html/inicio.php
@@ -13,15 +13,26 @@
         </div>
         <ul id="menu">
             <li><a href="index.php">inicio</a> </li>
-            <li><a href="index.php?accion=asignar">Asignar</a> </li>
+            <li class="activa"><a href="index.php?accion=asignar">Asignar</a> </li>
             <li><a href="index.php?accion=consultar">Consultar Cita</a> </li>
             <li><a href="index.php?accion=cancelar">Cancelar Cita</a> </li>
         </ul>
         <div id="contenido">
-            <h2>Título de página</h2>
-            <p>Contenido de la página</p>
+            <h2>Información General</h2>
+            <p>El Sistema de Gestión Odontológica permite administrar la información de los
+                pacientes,
+
+                tratamientos y citas a través de una interfaz web.</p>
+            <p>El sistema cuenta con las siguientes secciones:
+            <ul>
+                <li>Asignar cita</li>
+                <li>Consultar cita</li>
+                <li>Cancelar cita</li>
+            </ul>
+            </p>
         </div>
     </div>
+
 </body>
 
 </html>
